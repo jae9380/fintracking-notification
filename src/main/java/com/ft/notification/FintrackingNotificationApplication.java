7939d128ab1a0f1f5aba60ae88f59cbe0516c320
jae9380 <ljy5314@gmail.com>
2026-04-16 17:45:17 +0900

    Fintracking Notification Server

---
+++ src/main/java/com/ft/notification/FintrackingNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.ft.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackingNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackingNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ft/notification/FintrackingNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackingNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
